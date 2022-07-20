efb79a8dda6bd0e3fd31d7d2b0f1d1dd7c15fe57
beruki <berukii.z@live.jp>
2022-07-20 22:33:52 +0700

    button +/-

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -4,6 +4,7 @@ import React, { Component } from "react";
 import "./Track.css";
 
 class Track extends Component {
+
   renderAction() {
     if(this.props.isRemoval) {
       return <button className="Track-action">-</button>
@@ -20,7 +21,7 @@ class Track extends Component {
           <h3>{this.props.track.name}</h3>
           <p>{this.props.track.artist} | {this.props.track.album}</p>
         </div>
-        {this.renderAction}
+        {this.renderAction()}
       </div>
     )
   }
