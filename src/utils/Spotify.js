5a3155cd97050d4240b1e34efedf1fc3d8192bde
beruki <berukii.z@live.jp>
2022-07-21 12:11:49 +0700

    edited link

--- src/utils/Spotify.js
+++ src/utils/Spotify.js
@@ -1,6 +1,11 @@
 // TODO: Get Client ID from https://developer.spotify.com/dashboard/ and put it here
 const clientId = "d226d49115df452e8ca3afa1dc2fd663";
 
+const redirectUri = "http://localhost:5173/";
+const spotifyUrl = `https://accounts.spotify.com/authorize?response_type=token&scope=playlist-modify-public&client_id=${clientId}&redirect_uri=${redirectUri}`;
+let accessToken = undefined;
+let expiresIn = undefined;
+
 const Spotify = {
   getAccessToken() {
     if(accessToken) {
